fcda1234af68729b4557ca77c93d5ffed26bb7e4
Indra Yudha <indrayudha278@gmail.com>
2020-06-21 16:52:26 +0700

    update: Move Intent With Data

--- app/src/main/java/com/strivere/intentsample/MainActivity.kt
+++ app/src/main/java/com/strivere/intentsample/MainActivity.kt
@@ -9,14 +9,17 @@ import android.widget.Button
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var btnMoveActivity : Button
+    private lateinit var btnMoveActivityWithData : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnMoveActivity = findViewById(R.id.btn_move_activity)
+        btnMoveActivityWithData = findViewById(R.id.btn_move_activity_data)
 
         btnMoveActivity.setOnClickListener(this)
+        btnMoveActivityWithData.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
@@ -24,6 +27,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.btn_move_activity -> {
                 val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
                 startActivity(moveIntent)
+            }
+
+            R.id.btn_move_activity_data -> {
+                val moveIntentWithData = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                moveIntentWithData.putExtra(MoveWithDataActivity.EXTRA_NAME, "Someone")
+                moveIntentWithData.putExtra(MoveWithDataActivity.EXTRA_AGE, 5)
+                startActivity(moveIntentWithData)
             }
         }
     }
